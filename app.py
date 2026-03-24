a99ac67c927e5549f003fbe00bb933e1bee09c76
zavet-coder <fafafa09228@gmail.com>
2026-03-24 04:17:21 +0300

    updateall

--- app.py
+++ app.py
@@ -57,7 +57,6 @@ def create_order():
 
     return jsonify({"status": "success"})
 
+# В самом низу app.py оставь только это:
 if __name__ == "__main__":
-    # Локальный запуск (Railway проигнорирует этот блок и запустит через Gunicorn)
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
